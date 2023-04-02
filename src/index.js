ac59afa8a144f890183e6203467272636c699a75
zbys1av <zbyslav13@gmail.com>
2023-04-02 17:54:22 +0300

    some css changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './style.scss';
-
 console.log('Webpack starter with SCSS and dev server');
+
+const slidesContainer = document.getElementsByClassName("experts__list");
+const slide = document.querySelector(".card");
+
+const prevButton = document.getElementById("left");
+const nextButton = document.getElementById("right");
+
+nextButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
+});
