62fd7d0a450d990e8e4f88a86333a9626696c8d6
zbys1av <zbyslav13@gmail.com>
2023-04-11 01:23:34 +0300

    more js (not working really) + fixed header

--- src/index.js
+++ src/index.js
@@ -1,46 +1,56 @@
-// import Swiper from 'swiper';
-
 import Swiper, { Navigation, Pagination } from 'swiper';
-// import './style.scss';
+import './style.scss';
 
-// import Swiper from 'swiper/bundle';
-// import 'swiper/css/bundle';
+'use strict';       //check if code was written strictly to avoid any issues in different browsers
 
-import 'swiper/css/navigation';
-// import 'swiper/css/pagination';
+document.addEventListener('DOMContentLoaded', ()=> {    //js will run only when html loaded
+
+  const swiper = new Swiper('.swiper', {
+    modules: [Navigation, Pagination],
+    direction: 'horizontal',
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+
+  });
 
 
-// const swiper = new Swiper();
-// const swiper = new Swiper('.swiper', {
-//   modules: [Navigation, Pagination]
-// });
+//BURGER
+  const menuButton = document.querySelector(".burger");
+  const menuShow = document.querySelector(".header__list");
+  const body = document.querySelector("body");
+  const html = document.querySelector("html");
 
-const swiper = new Swiper('.swiper', {
+//BURGER__MENU
+  function toggleMenu() {
+    menuButton.classList.toggle('open');      //change to burger
+    menuShow.classList.toggle('show');        //hide menu
+    html.classList.toggle('scroll-hidden');   //disable scrolling page option
+    body.classList.toggle('scroll-hidden');   //disable scrolling page option
+  }
 
-  direction: 'horizontal',
-  // loop: true,
-
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+  if (menuButton) {         //check if element exists
+    menuButton.addEventListener("click", toggleMenu);
   }
 
 });
 
 
-//BURGER
-const menuButton = document.querySelector(".burger");
-const menuShow = document.querySelector(".header__list");
+//FAQ
+const faq = document.querySelectorAll(".faq__card");
+const question = document.querySelectorAll(".faq__question");
+const plus = document.querySelectorAll(".plus");
+const answer = document.querySelectorAll(".faq__text");
 
-//BURGER__MENU
-function toggleMenu(){
-  if (menuButton.classList.contains("open")){
-    menuButton.classList.remove('open');      //change to burger
-    menuShow.classList.remove('show');        //hide menu
-  } else {
-    menuButton.classList.add('open');         //change to "X"
-    menuShow.classList.add('show');           //show menu
-  }
+function showAnswer(){
+  faq.classList.toggle('selected-card-js');
+  question.classList.toggle('selected-question-js');
+  plus.classList.toggle('selected-question-js');
+  answer.classList.toggle('selected-text-js');
 }
 
-menuButton.addEventListener("click", toggleMenu);
+if (faq) {
+  faq.addEventListener("click", showAnswer);
+}
