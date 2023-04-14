098049b46370cbc7f337afb9f0cdc5120ce09ab5
ivanbatarin <ivan.batarin@boosta.co>
2023-04-14 14:00:32 +0200

    code review v3

--- src/index.js
+++ src/index.js
@@ -1,82 +1,83 @@
 import Swiper, { Navigation, Pagination } from 'swiper';
 import './style.scss';
 
-// import Swiper from 'swiper/bundle';
-// import 'swiper/css/bundle';
-
-import 'swiper/css/navigation';
-// import 'swiper/css/pagination';
-
-
-// const swiper = new Swiper();
-// const swiper = new Swiper('.swiper', {
-//   modules: [Navigation, Pagination]
-// });
+'use strict';
+document.addEventListener('DOMContentLoaded', ()=>{
 
   const swiper = new Swiper('.swiper', {
+
     modules: [Navigation, Pagination],
     direction: 'horizontal',
     loop: true,
+    slidesPerView: "auto",
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },      
     pagination: {
       el: '.swiper-pagination',
+      clickable: true,
     }
+
   });
 
 
-//BURGER
-  const menuButton = document.querySelector(".burger");
-  const menuShow = document.querySelector(".header__list");
-  const body = document.querySelector("body");
-  const html = document.querySelector("html");
+  //BURGER
+    const menuButton = document.querySelector(".burger");
+    const menuShow = document.querySelector(".header__list");
+    const body = document.querySelector("body");
+    const html = document.querySelector("html");
 
-//BURGER__MENU
-function toggleMenu(){
-    menuButton.classList.toggle('open');      //change to burger
-    menuShow.classList.toggle('show');        //hide menu
-}
+  //BURGER__MENU
+  function toggleMenu(){
+      menuButton.classList.toggle('open');      //change to burger
+      menuShow.classList.toggle('show');        //hide menu
+      html.classList.toggle('scroll-hidden');
+      body.classList.toggle('scroll-hidden');
+  }
 
-menuButton.addEventListener("click", toggleMenu);
+  if(menuButton && menuShow)  {
+    menuButton.addEventListener("click", toggleMenu);
+  }
 
-//FAQ
-//TRY THIS:
-//https://www.w3schools.com/howto/howto_js_accordion.asp
-const card = document.getElementsByClassName("faq__card");
-const question = document.getElementsByClassName(".faq__question");
-const text = document.getElementsByClassName(".faq__text");
-var i;
-var j = 0;
+  //FAQ
+  const card = document.querySelectorAll(".faq__card");
 
-for (i = 0; i < card.length; i++) {
-  card[i].addEventListener("click",openQuestion);
-}
+  if (card) {
+    card.forEach(e => {
+      e.addEventListener("click",openQuestion);
+    });
+  }
 
-function openQuestion(){
-  this.classList.toggle("open");
-  for (i = 0; i< card.length; i++){
-    if (this !== card[i]){
-      card[i].classList.remove('open');
+  function openQuestion(){
+    this.classList.toggle("open");
+    card.forEach(e => {
+      if (this !== e){
+        e.classList.remove('open');
+      }
+    });
+  }
+
+
+  //HOW TO
+  const readMore = document.querySelector(".how-to__button");
+  const addText = document.querySelector(".how-to__text");
+
+  function showMore(e){
+
+    e.preventDefault();
+
+    if (addText.classList.contains("show-text")){
+      readMore.innerHTML  = 'Read More';
+    } else {
+      readMore.innerHTML  = 'Show Less';
     }
+
+    addText.classList.toggle('show-text');
   }
-}
 
-
-//HOW TO
-const readMore = document.querySelector(".how-to__button");
-const addText = document.querySelector(".how-to__text-add");
-
-//чомусь при натиску на кнопку, сторінка прогортується в сам верх
-function showMore(){
-  if (addText.classList.contains("hide-text")){
-    addText.classList.remove('hide-text');  
-    readMore.innerHTML  = 'Show Less';
-  } else {
-    addText.classList.add('hide-text');  
-    readMore.innerHTML  = 'Read More';
+  if(readMore && addText) {
+    readMore.addEventListener("click", showMore);
   }
-}
 
-readMore.addEventListener("click", showMore);
+});
